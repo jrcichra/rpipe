bde72d10344fb287c979925e728130af43d41955
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2023-03-19 01:55:35 -0400

    cgo enabled 0 for static builds

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eou pipefail
 mkdir -p bin
-go build -v -o bin/rpipe cmd/rpipe/*.go
-go build -v -o bin/rpiped cmd/rpiped/*.go
+CGO_ENABLED=0 go build -v -o bin/rpipe cmd/rpipe/*.go
+CGO_ENABLED=0 go build -v -o bin/rpiped cmd/rpiped/*.go
